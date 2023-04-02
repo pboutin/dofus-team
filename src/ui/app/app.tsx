81a1b210a198a7a644d49f8d3d5355063bef13a1
Pascal Boutin <pboutin@medyx.com>
2023-04-02 18:09:21 -0400

    Setup daisy and fontawesome

--- src/ui/app/app.tsx
+++ src/ui/app/app.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Characters from 'ui/app/sections/characters/characters';
 
-const App = () => <div>Hello World! <Characters /></div>;
+const App = () => (
+  <div>
+    <div className="tabs">
+      <a className="tab tab-lifted">Tab 1</a> 
+      <a className="tab tab-lifted tab-active">Tab 2</a> 
+      <a className="tab tab-lifted">Tab 3</a>
+    </div>
+
+    <i className="fa-solid fa-dragon"></i>
+
+    <h1 className="text-3xl font-bold underline">Hello World</h1>
+
+    <Characters />
+  </div>
+);
 
 const rootElement = document.getElementById('root')!;
 createRoot(rootElement).render(<App />);
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/ui/**/*.tsx'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('daisyui')],
+};
