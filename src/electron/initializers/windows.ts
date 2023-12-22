162bb9f14b7239a63b6aff7d2d26958fb612b452
Pascal Boutin <pboutin@medyx.com>
2023-12-22 10:20:14 -0500

    Persist windows position

--- src/electron/initializers/windows.ts
+++ src/electron/initializers/windows.ts
@@ -1,4 +1,7 @@
 import { app, BrowserWindow } from "electron";
+import settings from "electron-settings";
+
+type PositionSetting = { x: number; y: number } | undefined;
 
 interface Context {
   debug: boolean;
@@ -14,13 +17,18 @@ let settingsBrowserWindow: BrowserWindow | null = null;
 let dashboardBrowserWindow: BrowserWindow | null = null;
 
 export const initializeWindows = ({ debug, onOpenedCallbacks }: Context) => {
-  const openSettings = () => {
+  const openSettings = async () => {
     if (settingsBrowserWindow) {
       settingsBrowserWindow.show();
       return settingsBrowserWindow;
     }
 
+    const position = (await settings.get(
+      "settings:position"
+    )) as PositionSetting;
+
     const browserWindow = new BrowserWindow({
+      ...(position ?? {}),
       height: 600,
       width: 800,
       resizable: false,
@@ -30,6 +38,11 @@ export const initializeWindows = ({ debug, onOpenedCallbacks }: Context) => {
     browserWindow.loadFile("./settings.html");
     browserWindow.setMenu(null);
     browserWindow.on("closed", () => (settingsBrowserWindow = null));
+
+    browserWindow.on("move", async () => {
+      const { x, y } = browserWindow.getBounds();
+      await settings.set("settings:position", { x, y });
+    });
 
     if (debug) {
       browserWindow.on("ready-to-show", () => {
@@ -43,13 +56,18 @@ export const initializeWindows = ({ debug, onOpenedCallbacks }: Context) => {
     return browserWindow;
   };
 
-  const openDashboard = () => {
+  const openDashboard = async () => {
     if (dashboardBrowserWindow) {
       dashboardBrowserWindow.show();
       return dashboardBrowserWindow;
     }
 
+    const position = (await settings.get(
+      "dashboard:position"
+    )) as PositionSetting;
+
     const browserWindow = new BrowserWindow({
+      ...(position ?? {}),
       height: 580,
       width: 600,
       resizable: false,
@@ -59,6 +77,11 @@ export const initializeWindows = ({ debug, onOpenedCallbacks }: Context) => {
     browserWindow.loadFile("./dashboard.html");
     browserWindow.setMenu(null);
     browserWindow.on("closed", () => app.quit());
+
+    browserWindow.on("move", async () => {
+      const { x, y } = browserWindow.getBounds();
+      await settings.set("dashboard:position", { x, y });
+    });
 
     if (debug) {
       browserWindow.on("ready-to-show", () => {
