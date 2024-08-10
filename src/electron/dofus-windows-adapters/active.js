5657a91bbb55ef44cd67e61e956beba548b50797
Pascal Boutin <pboutin@medyx.com>
2024-08-10 11:17:27 -0400

    Version the debug koffi adapter

---
+++ src/electron/dofus-windows-adapters/active.js
@@ -0,0 +1,9 @@
+const listDofusWindows = () => {
+  return ['Stub One', 'Stub Two', 'Stub Three', 'Stub Four', 'Stub Five'];
+};
+
+const focusDofusWindow = (characterToFocus) => {
+  console.log(`Focus "${characterToFocus}" called`);
+};
+
+module.exports = { listDofusWindows, focusDofusWindow };
