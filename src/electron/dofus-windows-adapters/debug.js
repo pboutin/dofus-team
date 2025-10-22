4807f72b07c0dced4715bd5c29bfe62a501458bc
Pascal Boutin <contact@pboutin.net>
2025-10-21 21:19:45 -0400

    Clean slate :boom:

--- src/electron/dofus-windows-adapters/debug.js
+++
@@ -1,9 +0,0 @@
-const listDofusWindows = () => {
-  return ['Stub One', 'Stub Two', 'Stub Three'];
-};
-
-const focusDofusWindow = (characterToFocus) => {
-  console.log(`Focus "${characterToFocus}" called`);
-};
-
-export default { listDofusWindows, focusDofusWindow };
--- src/themes.ts
+++
@@ -1,4 +0,0 @@
-import daisyuiThemes from '../daisyui-themes.json';
-
-export const fallback = 'dracula';
-export default daisyuiThemes;
